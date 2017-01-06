09c443b7776431cfededf93033d53167956a8f6a
oga <oga.ivc.s27@gmail.com>
2017-01-06 18:10:20 +0900

    Initial commit

---
+++ app/src/main/java/io/github/a0gajun/esareader/presentation/view/activity/BaseActivity.java
@@ -0,0 +1,11 @@
+package io.github.a0gajun.esareader.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Junya on 1/6/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/io/github/a0gajun/esareader/presentation/view/activity/PostListActivity.java
@@ -0,0 +1,15 @@
+package io.github.a0gajun.esareader.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.a0gajun.esareader.R;
+
+public class PostListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
